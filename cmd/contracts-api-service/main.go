f35aa37cdcc18f7ca573a6ab8b6075fadcf34247
thepikart <114285877+thepikart@users.noreply.github.com>
2026-05-03 14:55:09 +0200

    init

---
+++ cmd/contracts-api-service/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "log"
+    "os"
+    "strings"
+    "github.com/gin-gonic/gin"
+    "github.com/thepikart/contracts-webapi/api"
+)
+
+func main() {
+    log.Printf("Server started")
+    port := os.Getenv("CONTRACTS_API_PORT")
+    if port == "" {
+        port = "8080"
+    }
+    environment := os.Getenv("CONTRACTS_API_ENVIRONMENT")
+    if !strings.EqualFold(environment, "production") { // case insensitive comparison
+        gin.SetMode(gin.DebugMode)
+    }
+    engine := gin.New()
+    engine.Use(gin.Recovery())
+    // request routings
+    engine.GET("/openapi", api.HandleOpenApi)
+    engine.Run(":" + port)
+}
